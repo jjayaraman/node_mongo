285a4820fe2e2e7cef568e92cfa7419a18d3e6f7
jjayaraman <india.jai@gmail.com>
2021-03-29 14:00:18 +0100

    handle bar example

--- simple-hb/routes/users.js
+++ simple-hb/routes/users.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  res.send('users...');
+});
+
+router.get('/hello', function (req, res, next) {
+  var data = { dynamic: "....a dynamic text.." }
+  res.render("hello", data)
 });
 
 module.exports = router;
